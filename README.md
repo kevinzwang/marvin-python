d9c3e39363c8a6a9306bdf2ef8e550e2c33bc0ef
Kevin Wang <kevinzhiwang@gmail.com>
2017-06-15 09:59:13 -0700

    first setup
    yeah basically nothing is done but whatever ya know

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chaos-bot
 A bot to manage the Discord Chaos server.
+
+Currently only on Python 3.5.*.
+
+To use, first do:
+```
+pip install discord.py[voice]
+pip install PyYaml
+```
+
+Also, add a data.yaml in the source folder with the bot token in it.
---
+++ tests/message.py
@@ -0,0 +1,18 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('!test'):
+		await client.send_message(message.channel, 'aaron stop being an idiot')
+
+client.run('token')
